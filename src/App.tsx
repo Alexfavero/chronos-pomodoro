751103d1ab900c496a7c17b0ab1e0ba482b37997
Alex Abreu <alexfavero4036@gmail.com>
2025-08-20 16:33:41 -0300

    add buttons components, no logic yet

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -45,7 +47,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
           </div>
         </form>
       </Container>
